1d029439310fad5a66c33875ab3b155c3878e78d
Liwei Ma <Liwei.Ma@gmail.com>
2022-08-30 09:49:24 +0800

    use public_name

--- foo/bin/main.ml
+++ foo/bin/main.ml
@@ -1,3 +1,8 @@
+(* let () = print_endline "Hello, World!" *)
+(*let () = print_hello *)
+
+open Bar
 
 let () = Printf.printf "hello world\n"
 let () = Printf.printf "hello world\n"
+let () = LibBar.print_hello()
