5b822c847a429afc25e9253f38fbfadc92e8e1f1
Liwei Ma <liwei.ma@gmail.com>
2022-09-11 17:12:16 +0800

    try merlin mode

--- foo/bin/main.ml
+++ foo/bin/main.ml
@@ -6,3 +6,9 @@ open Bar
 let () = Printf.printf "hello world\n"
 let () = Printf.printf "hello world\n"
 let () = LibBar.print_hello()
+let () =
+  let a = [1; 2; 3; 4] in
+  let b = List.append a a in
+  Printf.printf "%s" (String.concat " " (List.map string_of_int b))
+let () =
+  Lwt_main.run (Lwt_io.printf "Hello World from lwt\n")
